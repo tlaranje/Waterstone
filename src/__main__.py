8e246dd5f9f8cf953598a91a762546045fea34dd
tlaranje <cydogbi10@gmail.com>
2026-04-16 23:58:51 +0100

    Add code to create a window to serve as an overlay

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,45 @@
-import os
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtCore import Qt
+from rich import print
 import glob
+import sys
+import os
 
 
-def update_log_data():
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Escape:
+            self.close()
+
+
+def create_window() -> None:
+    app = QApplication(sys.argv)
+
+    window = QMainWindow()
+    window.show()
+
+    window = MainWindow()
+
+    screen = QGuiApplication.primaryScreen()
+    assert screen is not None
+    rect = screen.geometry()
+
+    width = rect.width()
+    height = rect.height()
+
+    window.setFixedSize(width, height)
+
+    window.show()
+    app.exec()
+
+
+def read_log_data():
     base_path = ""
 
     try:
@@ -28,4 +65,4 @@ def update_log_data():
 
 
 if __name__ == "__main__":
-    update_log_data()
+    create_window()
